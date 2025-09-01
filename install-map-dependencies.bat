a39863f018e8f5bd5666c374e79ac5c4024bec94
Srinivas22-crypto <potharajusrinivas05@gmail.com>
2025-09-01 22:05:01 +0530

    modified frontend files

--- install-map-dependencies.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing map dependencies...
-cd frontend
-npm install leaflet react-leaflet @types/leaflet
-echo Map dependencies installed successfully!
-pause
--- seed-trips.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Seeding trip data...
-cd backend
-node scripts/seedTrips.js
-echo Trip seeding completed!
-pause
